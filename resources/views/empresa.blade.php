30c47862d5ec900fa211adc60707e0b2d1e60cca
gabrielmodesto <gabriel.modesto@nexttecnologiadainformacao.com.br>
2019-02-03 21:06:27 -0200

    Studying routes

---
+++ resources/views/empresa.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Curso Básico de Laravel</title>
+</head>
+<body>
+    <h1>View Representando a empresa</h1>
+</body>
+</html>
