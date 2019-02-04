b7ef26bdf5a77a271f277584904aef2462a8bb8f
gabrielmodesto <gabriel.modesto@nexttecnologiadainformacao.com.br>
2019-02-03 23:58:55 -0200

    Route with param

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,14 @@
 <?php
 
+Route::get('/categoria/{idCategoria}', function($idCategoria){
+    return "Post da categoria {$idCategoria}";
+});
+
+
+Route::get('/categoria/{idCategoria}', function($idCategoria){
+    return "Post da categoria {$idCategoria}";
+});
+
 Route::get('/nome/nome2/nome3', function(){
     return ('Rota nomeada');
 })->name('rota.nomeada');
