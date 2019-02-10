199c966a94a23cc71e50cb6cd319bae4643bc2ef
gabrielmodesto <gabriel.modesto@nexttecnologiadainformacao.com.br>
2019-02-10 20:51:01 -0200

    Controller para site, rota para index e contato implementado

---
+++ app/Http/Controllers/SiteController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SiteController extends Controller
+{
+    public function index(){
+        return 'Home Page do Site';
+    }
+    public function contato(){
+        return "Page Contato";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,36 +1,7 @@
 <?php
-//
-////Rotas em grupo
-//Route::group(['prefix' => 'painel', 'middleware' => 'auth'], function(){
-//    Route::get('/grupo1', function(){
-//        return 'Grupo 1';
-//    });
-//    Route::get('/grupo2', function(){
-//        return 'Grupo 2';
-//    });
-//});
-//Route::get('/login', function(){
-//    return ("#form Login");
-//});
-////sem passar o parametro na url
-//Route::get('/categoria2/{idCategoria?}', function($idCategoria=1){
-//    return "Post da categoria {$idCategoria}";
-//});
-////passando o parametro na url
-//Route::get('/categoria/{idCategoria}', function($idCategoria){
-//    return "Post da categoria {$idCategoria}";
-//});
-////rota nomeada
-//Route::get('/nome/nome2/nome3', function(){
-//    return ('Rota nomeada');
-//})->name('rota.nomeada');
-//
-//Route::get('/contato', function(){
-//    return view('contato');
-//});
-//Route::get('/empresa', function(){
-//   return view('empresa');
-//});
-Route::get('/', function () {
-    return redirect()->route('rota.nomeada');
-});
+
+//utilizando parametros no rota
+
+Route::get('/contato', 'SiteController@contato');
+Route::get('/', 'SiteController@index');
+
