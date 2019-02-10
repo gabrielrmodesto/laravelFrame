2f11483c02c4cc5a576149df027f380b56dc516a
gabrielmodesto <gabriel.modesto@nexttecnologiadainformacao.com.br>
2019-02-10 20:34:53 -0200

    Rotas modificadas

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,36 @@
 <?php
-
-Route::get('/categoria/{idCategoria}', function($idCategoria){
-    return "Post da categoria {$idCategoria}";
-});
-
-
-Route::get('/categoria/{idCategoria}', function($idCategoria){
-    return "Post da categoria {$idCategoria}";
-});
-
-Route::get('/nome/nome2/nome3', function(){
-    return ('Rota nomeada');
-})->name('rota.nomeada');
-
-Route::get('/contato', function(){
-    return view('contato');
-});
-Route::get('/empresa', function(){
-   return view('empresa');
-});
+//
+////Rotas em grupo
+//Route::group(['prefix' => 'painel', 'middleware' => 'auth'], function(){
+//    Route::get('/grupo1', function(){
+//        return 'Grupo 1';
+//    });
+//    Route::get('/grupo2', function(){
+//        return 'Grupo 2';
+//    });
+//});
+//Route::get('/login', function(){
+//    return ("#form Login");
+//});
+////sem passar o parametro na url
+//Route::get('/categoria2/{idCategoria?}', function($idCategoria=1){
+//    return "Post da categoria {$idCategoria}";
+//});
+////passando o parametro na url
+//Route::get('/categoria/{idCategoria}', function($idCategoria){
+//    return "Post da categoria {$idCategoria}";
+//});
+////rota nomeada
+//Route::get('/nome/nome2/nome3', function(){
+//    return ('Rota nomeada');
+//})->name('rota.nomeada');
+//
+//Route::get('/contato', function(){
+//    return view('contato');
+//});
+//Route::get('/empresa', function(){
+//   return view('empresa');
+//});
 Route::get('/', function () {
     return redirect()->route('rota.nomeada');
 });
