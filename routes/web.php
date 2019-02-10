bd4be667a00926b28414ff8e332af409b4a9b64e
gabrielmodesto <gabriel.modesto@nexttecnologiadainformacao.com.br>
2019-02-10 21:20:56 -0200

    Middleware para autenticação

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 Route::group(['namespace' => 'Site'], function(){
     //utilizando parametros no rota
-    Route::get('/categoria/{id}', 'SiteController@categoria');
+    Route::get('/categoria/{id}', 'SiteController@categoria')->middleware('auth');
     //parametro opcional
     Route::get('/categoriaOp/{id?}', 'SiteController@categoriaOp');
     Route::get('/contato', 'SiteController@contato');
