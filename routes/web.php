3e92ed0a2eaabdad2d95d256342d893997214d6a
gabrielmodesto <gabriel.modesto@nexttecnologiadainformacao.com.br>
2019-02-10 21:10:00 -0200

    Utilizando o group para manter todas as rotas do mesmo namespace

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
-//utilizando parametros no rota
-Route::get('/categoria/{id}', 'SiteController@categoria');
-//parametro opcional
-Route::get('/categoriaOp/{id?}', 'SiteController@categoriaOp');
-Route::get('/contato', 'SiteController@contato');
-Route::get('/', 'SiteController@index');
+Route::group(['namespace' => 'Site'], function(){
+    //utilizando parametros no rota
+    Route::get('/categoria/{id}', 'SiteController@categoria');
+    //parametro opcional
+    Route::get('/categoriaOp/{id?}', 'SiteController@categoriaOp');
+    Route::get('/contato', 'SiteController@contato');
+    Route::get('/', 'SiteController@index');
+});
 
