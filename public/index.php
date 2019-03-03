2fc792a9a9da5f812f2cf254cf97f1d3769edaa8
gabrielmodesto <gabriel.modesto@nexttecnologiadainformacao.com.br>
2019-03-03 17:11:59 -0300

    Estrutura para o projeto

--- public/index.php
+++ public/index.php
@@ -19,7 +19,7 @@
 |
 */
 
-require __DIR__ . '/especializati/bootstrap/autoload.php';
+require __DIR__ . '/../bootstrap/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -33,7 +33,7 @@ require __DIR__ . '/especializati/bootstrap/autoload.php';
 |
 */
 
-$app = require_once __DIR__ . '/especializati/bootstrap/app.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
