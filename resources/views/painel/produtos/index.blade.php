08e793b288e5913a59356aa85b46faac5a54997c
gabrielmodesto <gabriel.modesto@nexttecnologiadainformacao.com.br>
2019-03-04 19:43:29 -0300

    Estilização da página como um todo

--- resources/views/painel/produtos/index.blade.php
+++ resources/views/painel/produtos/index.blade.php
@@ -1,12 +1,16 @@
 @extends('painel.templates.template')
 @section('contentList')
-    <h2>Listagem de produtos</h2>
-
-    <table>
+    <h2 class="title-list">Listagem de produtos</h2>
+    <a href="" class="btn btn-primary btnCadastro">
+        <i class="fas fa-plus"></i>
+        Cadastrar
+    </a>
+    <table class="table table-striped">
         <thead>
         <tr>
             <th>Nome</th>
             <th>Description</th>
+            <th>Ações</th>
         </tr>
         </thead>
         <tbody>
@@ -17,6 +21,10 @@
                 </td>
                 <td>
                     {{$produtos->description}}
+                </td>
+                <td class="actions">
+                    <a href=""><i class="fas fa-pencil-alt"></i></a>
+                    <a href=""><i class="fas fa-trash-alt"></i></a>
                 </td>
             </tr>
         @endforeach
--- resources/views/painel/templates/template.blade.php
+++ resources/views/painel/templates/template.blade.php
@@ -5,7 +5,10 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{$title or 'Painel Curso'}}</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{url('assets/painel/css/style.css')}}">
+
 </head>
 <body>
     <div class="container">
